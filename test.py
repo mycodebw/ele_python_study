16e3efbb9d6a483a0e55dd0b275adf6cd7a4c6ae
BianWei <wei.bian@ele.me>
2017-11-09 10:44:41 +0800

    test.py

--- test.py
+++ test.py
@@ -23,3 +23,17 @@ class TestClass:
     def test_two(self):
         x = "hello"
         assert x == "hi"
+
+class Dict(dict):
+
+    def __init__(self, **kw):
+        super(Dict, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+            self[key] = value
