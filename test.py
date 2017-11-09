644fcbf4d5ed5189b733c98f049a32eb702a92dc
BianWei <wei.bian@ele.me>
2017-11-09 11:08:57 +0800

    test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import unittest
 #pl = input("请输入年份")
 #
 #year = int(pl)
@@ -7,33 +8,54 @@
 
 #coding=utf-8
 
-def func(x):
-    return x + 1
+#def func(x):
+#    return x + 1
+#
+#def test_answer():
+#    assert func(3) == 5
+#
+#
+#class TestClass:
+#
+#    def test_one(self):
+#        x = "this"
+#        assert "h" in x
+#
+#    def test_two(self):
+#        x = "hello"
+#        assert x == "hi"
+#
+#class Dict(dict):
+#
+#    def __init__(self, **kw):
+#        super(Dict, self).__init__(**kw)
+#
+#    def __getattr__(self, key):
+#        try:
+#            return self[key]
+#        except KeyError:
+#            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+#
+#    def __setattr__(self, key, value):
+#            self[key] = value
 
-def test_answer():
-    assert func(3) == 5
 
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.num = input('Enter a number: ')
 
-class TestClass:
+    def test_case1(self):
+        print (self.number)
+        self.assertEqual(self.number,10,msg='Your input is not 10')
 
-    def test_one(self):
-        x = "this"
-        assert "h" in x
+    def test_case2(self):
+        print (self.number)
+        self.assertEqual(self.number,20,msg='Your input is not 20')
 
-    def test_two(self):
-        x = "hello"
-        assert x == "hi"
+    @unittest.skip
+    def test_case3(self):
+        print (self.number)
+        self.assertEqual(self.number,30,msg='Your input is not 30')
 
-class Dict(dict):
-
-    def __init__(self, **kw):
-        super(Dict, self).__init__(**kw)
-
-    def __getattr__(self, key):
-        try:
-            return self[key]
-        except KeyError:
-            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
-
-    def __setattr__(self, key, value):
-            self[key] = value
+    if __name__ == '__main__':
+        unittest.main()
