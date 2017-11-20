3f4c7d41cb8995ccf713e59b7abc086b9d8ff7b8
BianWei <wei.bian@ele.me>
2017-11-20 12:02:11 +0800

    unit2_new.py

--- unit2.py
+++ unit2.py
@@ -1,14 +1,20 @@
 from widget import Widget
 import unittest
-class TestWidget:
+class WidgetTestCase(unittest.TestCase):
+    def setUp(self):
+        self.widget = Widget()
+    def tearDown(self):
+        self.widget.dispose()
+        self.widget = None
     def testSize(self):
-        expectedSize = (40, 40);
-        widget = Widget()
-        if widget.getSize() == expectedSize:
-            print ("test [Widget]: getSize works perfected!")
-        else:
-            print ("test [Widget]: getSize doesn't work!")
-
-if __name__ == '__main__':
-    myTest = TestWidget()
-    myTest.testSize()
+        self.assertEqual(self.widget.getSize(), (40, 40))
+    def testResize(self):
+        self.widget.resize(100, 100)        
+        self.assertEqual(self.widget.getSize(), (100, 100))        
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    suite.addTest(WidgetTestCase("testSize"))
+    suite.addTest(WidgetTestCase("testResize"))
+    
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
