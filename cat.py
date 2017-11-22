d2cd643336d9709ce454c9adb176a0ea46abb030
BianWei <wei.bian@ele.me>
2017-11-22 17:27:24 +0800

    异常处理-1

--- cat.py
+++ cat.py
@@ -1,18 +1,27 @@
 import os
-try:
-    fh = open("testfile", "r")
-    fh.write("这是一个测试文件，用于测试异常!!")
-except IOError:
-    print ("Error: 没有找到文件或读取文件失败")
-else:
-    print ("内容写入文件成功")
-    fh.close()
+#try:
+#    fh = open("testfile", "r")
+#    fh.write("这是一个测试文件，用于测试异常!!")
+#except IOError:
+#    print ("Error: 没有找到文件或读取文件失败")
+#else:
+#    print ("内容写入文件成功")
+#    fh.close()
 
-class error(tryError):
+class error(OSError):
     def __init__(self, arg):
         self.args = arg
 
 try:
     raise error("Bad file")
-except error,e:
+except error as e:
     print (e.args)
+
+
+import os, sys
+
+path = ".daily/aa/bb/cc"
+
+os.makedirs( path, 755 );
+
+print ("file created")
