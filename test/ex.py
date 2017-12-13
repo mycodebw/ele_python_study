175c6a9d1148612225309a169fd24e867b3de51c
BianWei <wei.bian@ele.me>
2017-12-13 15:35:31 +0800

    exception

--- test/ex.py
+++ test/ex.py
@@ -1,4 +1,5 @@
 class MyException(Exception):
+    """自定义异常类"""
     def __init__(self,message):
         Exception.__init__(self)
         self.message=message   
@@ -6,6 +7,7 @@ class MyException(Exception):
 a=input("please input a num:")
 
 if a < "20":
+    """如果小于20捕获异常,否则什么都不返回,类只定义没信息所以什么都没有"""
     try:
         raise MyException("my excepition is raised ")
     except MyException as e:
