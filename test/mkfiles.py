4aed16d82147638848e8d123d89f349eadf88445
BianWei <wei.bian@ele.me>
2017-12-13 15:31:08 +0800

    read files

--- test/mkfiles.py
+++ test/mkfiles.py
@@ -10,12 +10,14 @@ crfile = []
 
 def create_mk_files(numbers):
     try:
-
+        """循环创建文件"""
         for num in range(numbers):
             os.mkdir(path + dirname +str(num))
     except OSError:
+        """异常捕获"""
         print ("目录已经存在")
     else:
+        """用startwith判断test开头的文件写入test_write"""
         for dirs in os.listdir(path):
             if dirs.startswith('test'):
                 op = (path + dirs + '/' + filename)
@@ -28,6 +30,7 @@ def create_mk_files(numbers):
 
 def read_files():
     try:
+        """循环读取然后strip去空格"""
         for dirs in os.listdir(path):
             if dirs.startswith('test'):
                 rdfiles = (path + dirs + '/' + filename)
@@ -36,6 +39,7 @@ def read_files():
                     line = line.strip() 
                     print (line)
     except OSError:
+        """异常捕获文件不存在打印"""
         print ("不存在读取失败")
     finally:
         print ("Close")
